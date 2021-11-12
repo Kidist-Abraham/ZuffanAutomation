b7e5233c6725908e74add0b0fb0c48a9e82416a1
Kidist-Abraham <kidistabraham@gmail.com>
2021-11-12 16:31:05 +0000

    wip

--- cron-cert.sh
+++ cron-cert.sh
@@ -3,5 +3,5 @@ today=$(date +'%Y-%m-%d')
 domain=$1
 sudo domain=$domain today=$today -u ubuntu crontab<<EOF
 $(crontab -l)
-0 3 * * * bash -c '(( \$(date +\%s) / 86400 \% 75 == (( \$(date +\%s -d "$${today}") / 86400 \% 75 )) )) && sudo certbot renew --cert-name $${domain} && sudo systemctl restart nginx'
+0 3 * * * bash -c '(( \$(date +\%s) / 86400 \% 75 == (( \$(date +\%s -d "${today}") / 86400 \% 75 )) )) && sudo certbot renew --cert-name ${domain} && sudo systemctl restart nginx'
 EOF
