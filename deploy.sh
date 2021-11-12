b7174f7d82d78d584817f35c1538c5bdda04662f
Kidist-Abraham <kidistabraham@gmail.com>
2021-11-12 12:56:02 +0000

    add scripts to automate web deployment

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+while [ $# -gt 0 ]; do
+
+   if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+   fi
+
+  shift
+done
+
+if [[ -z "$domain" ]]; then
+    echo "Must provide the domain." 1>&2
+    exit 1
+fi
+
+if [[ -z "$file" ]]; then
+    echo "Must provide the file." 1>&2
+    exit 1
+fi
+
+
+IMAGE=ubuntu/apache2:2.4-21.04_beta
+
+
+docker run -d --name $file  -v /home/ubuntu/data/web/$file/:/var/www/html -e TZ=UTC -p 80 $IMAGE
+
+port=$(docker inspect -f '{{ (index (index .NetworkSettings.Ports "80/tcp") 0).HostPort }}' $file)
+
+bash /home/ubuntu/scripts/set_up_proxy.sh --domain $domain --port $port
---
+++ set_up_proxy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+while [ $# -gt 0 ]; do
+
+   if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+   fi
+
+  shift
+done
+
+if [[ -z "$domain" ]]; then
+    echo "Must provide the domain." 1>&2
+    exit 1
+fi
+
+if [[ -z "$port" ]]; then
+    echo "Must provide the port." 1>&2
+    exit 1
+fi
+
+
+sudo certbot certonly --standalone -d $domain --email kidistabraham@gmail.com -n --agree-tos
+sudo touch /etc/nginx/sites-available/$domain
+
+sudo domain=$domain port=$port bash -c 'cat <<EOT > /etc/nginx/sites-available/${domain}
+server {
+  listen        443 ssl;
+  server_name  ${domain};
+    ssl_certificate /etc/letsencrypt/live/${domain}/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/${domain}/privkey.pem;
+  location / {
+    proxy_pass  http://localhost:${port};
+  }
+}
+EOT'
+
+sudo rm /etc/nginx/sites-enabled/${domain} 
+
+sudo ln -s /etc/nginx/sites-available/${domain} /etc/nginx/sites-enabled/
+
+sudo systemctl restart nginx
+
+echo "Finish setting up"
