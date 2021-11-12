83104aa7bd7aad471976b910e995ea35f0d307fc
Kidist-Abraham <kidistabraham@gmail.com>
2021-11-12 16:38:44 +0300

    add cert renewal script

---
+++ cron-cert.sh
@@ -0,0 +1,7 @@
+# Setup a cron job for cert renewal. The cron job is schedules to run every 75 days starting from the day this script is running.
+today=$(date +'%Y-%m-%d')
+domain=$1
+sudo -u ubuntu crontab<<EOF
+$(crontab -l)
+0 3 * * * bash -c '(( \$(date +\%s) / 86400 \% 75 == (( \$(date +\%s -d "$${today}") / 86400 \% 75 )) )) && sudo certbot renew --cert-name $${domain} && sudo systemctl restart nginx'
+EOF
