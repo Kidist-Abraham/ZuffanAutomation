f8f46ccbb2e0f9375ee995ae449f30d93ecdc858
Kidist-Abraham <kidistabraham@gmail.com>
2021-11-21 23:59:33 +0300

    use pwd for relative path

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ fi
 
 if [[ "$temp" == "true" ]]
 then
-bash create-domain.sh --domain $domain 
+bash $pwd/create-domain.sh --domain $domain 
 fi
 
 IMAGE=ubuntu/apache2:2.4-21.04_beta
@@ -38,4 +38,4 @@ docker run -d --name $file  -v /home/ubuntu/data/web/$file/:/var/www/html -e TZ=
 
 port=$(docker inspect -f '{{ (index (index .NetworkSettings.Ports "80/tcp") 0).HostPort }}' $file)
 
-bash set_up_proxy.sh --domain $domain --port $port
+bash $pwd/set_up_proxy.sh --domain $domain --port $port
--- set_up_proxy.sh
+++ set_up_proxy.sh
@@ -44,6 +44,6 @@ sudo ln -s /etc/nginx/sites-available/${domain} /etc/nginx/sites-enabled/
 
 sudo systemctl restart nginx
 
-bash cron-cert.sh  $domain 
+bash $pwd/cron-cert.sh  $domain 
 
 echo "Finish setting up"
